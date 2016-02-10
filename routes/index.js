5addb4d7ed3c444e4ac292851de070cc89c1e208
John Martell <jnmartell2007@gmail.com>
2016-02-10 14:03:55 -0600

    Mailgun

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,21 @@ var Mailgun = require('mailgun').Mailgun;
 var mg = new Mailgun(process.env.MAILGUN_API_KEY);
 
 function sendMail(recip, obj) {
-      mg.sendRaw('john@johnmartell.net',
-        [recip],
-        'From: john@johnmartell.net' +
-        '\nTo: ' + recip +
-        '\nContent-Type: text/html; charset=utf-8' +
-        '\nSubject: Thanks for your RSVP' +
-        "\n\n<html><body><h3>Thanks for your RSVP!</h3><div><b>Name:</b> " + obj.name + "</div><div><b>Email:</b> " + obj.email + "</div><div><b>Attending:</b> " + obj.attending + "</div><div><b>Dinner Option:</b> " + obj.dinnerOption +"</div></body></html>",
-        function(err) {
-            err && console.log(err)
-        });
+     try {
+         mg.sendRaw('john@johnmartell.net',
+             [recip],
+             'From: john@johnmartell.net' +
+             '\nTo: ' + recip +
+             '\nContent-Type: text/html; charset=utf-8' +
+             '\nSubject: Thanks for your RSVP' +
+             "\n\n<html><body><h3>Thanks for your RSVP!</h3><div><b>Name:</b> " + obj.name + "</div><div><b>Email:</b> " + obj.email + "</div><div><b>Attending:</b> " + obj.attending + "</div><div><b>Dinner Option:</b> " + obj.dinnerOption + "</div></body></html>",
+             function (err) {
+                 err && console.log(err)
+             });
+     }catch(e) {
+         console.log("MAIL SENDING ERROR: " + e);
+         console.log(JSON.stringify(obj));
+     }
 }
 
 
@@ -28,7 +33,7 @@ router.post('/rsvp', function(req,res) {
   var json = req.body;
   var mongoObject = {
     name : encodeURIComponent(json.name) || "NOT PROVIDED",
-    email : encodeURIComponent(json.email) || "NOT PROVIDED",
+    email : json.email || "NOT PROVIDED",
     dinnerOption : encodeURIComponent(json.dinnerOption) || "NOT PROVIDED",
     attending : !!json.attending
   };
