80651d71e6770e8c97b9f603582c961856e05dbb
John Martell <jnmartell2007@gmail.com>
2016-02-10 13:23:45 -0600

    Mailgun

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+var Mailgun = require('mailgun').Mailgun;
+var mg = new Mailgun(process.env.MAILGUN_API_KEY);
+
+function sendMail(recip, obj) {
+  sendText('john@johnmartell.net',
+      [recip],
+      "Thanks for your RSVP",
+      "<html><body><h3>Thanks for your RSVP!</h3><div><b>Name:</b> " + obj.name + "</div><div><b>Email:</b> " + obj.email + "</div><div><b>Attending:</b> " + obj.attending + "</div><div><b>Dinner Option:</b> " + obj.dinnerOption +"</div></body></html>",
+      {},
+      function(err) {
+        err && console.log(err)
+      });
+}
+
 
 router.get('/', function(req,res) {
   res.sendfile('public/index.html');
@@ -16,6 +30,9 @@ router.post('/rsvp', function(req,res) {
     dinnerOption : encodeURIComponent(json.dinnerOption) || "NOT PROVIDED",
     attending : !!json.attending
   };
+
+  sendMail("jnmartell2007@gmail.com",mongoObject);
+  sendMail(obj.email,mongoObject);
 
   MongoClient.connect(url, function(err, db) {
     db.collection('rsvp').insertOne(mongoObject);
